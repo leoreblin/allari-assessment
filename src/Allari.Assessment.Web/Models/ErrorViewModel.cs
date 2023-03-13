243ab51140db143f078d38dd4bd5f7b214990302
Leonardo Reblin <lreblin@neowrk.com>
2023-03-12 22:53:07 -0300

    Creating an MVC project

---
+++ src/Allari.Assessment.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Allari.Assessment.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
